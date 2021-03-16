7ccd60f8f051a524ab4a26ee8ca2fbff82789d11
Nick Bourdakos <nicholas.bourdakos@ibm.com>
2021-03-15 23:31:10 -0400

    Add some styles

--- packages/pipeline-editor/src/PipelineEditor/index.tsx
+++ packages/pipeline-editor/src/PipelineEditor/index.tsx
@@ -36,7 +36,6 @@ import {
 } from "@elyra/canvas";
 import { IntlProvider } from "react-intl";
 import {
-  css,
   DefaultTheme,
   ThemeProvider as InternalThemeProvider,
   useTheme,
@@ -47,6 +46,7 @@ import PalettePanel from "../PalettePanel";
 import PipelineController from "../PipelineController";
 import PropertiesPanel from "../PropertiesPanel";
 import SplitPanelLayout from "../SplitPanelLayout";
+import { CanvasOverrides } from "../styles";
 import TabbedPanelLayout from "../TabbedPanelLayout";
 import useBlockEvents from "./useBlockEvents";
 
@@ -647,7 +647,7 @@ const defaultTheme: DefaultTheme = {
   typography: {
     fontFamily: "-apple-system, system-ui, sans-serif",
     fontWeight: "normal",
-    fontSize: 13,
+    fontSize: "13px",
   },
 };
 
@@ -656,11 +656,6 @@ function createTheme(overides: Partial<DefaultTheme>) {
 }
 
 const theme = {};
-
-const x = css`
-  color: var(--fun-day);
-  background-color: ${(props) => props.theme.palette.primary.main};
-`;
 
 const ThemeProvider: React.FC<{ theme: Partial<DefaultTheme> }> = ({
   theme,
@@ -677,6 +672,7 @@ function ThemedPipelineEditor(props: Props) {
   return (
     <ThemeProvider theme={theme}>
       <InternalThemeProvider theme={createTheme}>
+        <CanvasOverrides />
         <PipelineEditor {...props} />
       </InternalThemeProvider>
     </ThemeProvider>
